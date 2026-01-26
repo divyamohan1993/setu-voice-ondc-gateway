27c0f26fe3ee814e836fcdc691da7e963ecfb43c
Divya Mohan <divyamohan1993@users.noreply.github.com>
2026-01-27 00:18:04 +0530

    Auto-sync: 27-Jan-26  0:18:04.35

--- autosync.bat
+++ autosync.bat
@@ -6,7 +6,7 @@ setlocal EnableDelayedExpansion
 :: ==========================================
 set "REPO_URL=https://github.com/divyamohan1993/setu-voice-ondc-gateway.git"
 set "BRANCH=main"
-set "SYNC_INTERVAL=30"
+set "SYNC_INTERVAL=15"
 
 :: Navigate to the directory where this script is located
 cd /d "%~dp0"
